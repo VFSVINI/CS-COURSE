7cfb323d9aea68e73f8c4415b1aa08da4b5849f8
VFSVINI <vfs2@cin.ufpe.br>
2026-08-30 20:32:36 -0300

    Primeira Aplicação

---
+++ NamespacesEX1.cs
@@ -0,0 +1,20 @@
+namespace ProgramaPrincipal{
+
+    public class ClasseUm
+    {
+
+        public int Id {get; set;}
+
+        public string Nome {get; set;}
+
+    }    
+
+    public class ClasseUm  
+    {
+
+        public int Id {get; set;}
+
+        public string Nome {get; set;}
+    }  
+    
+}
