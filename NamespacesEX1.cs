ee9d17a1c1ddb0f8e774b638ff0dcea2ec8ad7a8
VFSVINI <vfs2@cin.ufpe.br>
2026-08-30 20:52:04 -0300

    Uso do Namespace

--- NamespacesEX1.cs
+++ NamespacesEX1.cs
@@ -1,3 +1,13 @@
+// Na figura acima temos duas classes com o nome de “ClasseUm”.
+// Isso não é permitido na estrutura da linguagem e o compilador vai exibir um erro
+// Informando que já existe uma classe com o mesmo nome
+// Assim como em qualquer sistema operacional
+// Dentro de uma mesma estrutura não será possível
+// Ter um arquivo com o mesmo nome
+// Assim que alterarmos o nome da segunda classe para algo diferente
+// O compilador entenderá que não temos mais conflitos de membros dentro do mesmo namespace
+
+
 namespace ProgramaPrincipal{
 
     public class ClasseUm
