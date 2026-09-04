383eee25db4c67546186f10ca5a62c9b538b217f
VFSVINI <vfs2@cin.ufpe.br>
2026-09-04 18:18:22 -0300

    Trabalhando Com Variáveis

---
+++ VariaveisEX1.cs
@@ -0,0 +1,22 @@
+// As variáveis são espaços na memória do computador
+// Onde podemos salvar, manipular e exibir informações
+
+namespace SistemaBancario {
+    
+  public class InstanciaObjeto {
+    
+    public static void Main()
+        {
+            var objetoGerente = new Gerente();
+        }
+  }  
+}
+
+// A palavra reservada var tem a função de informar o sistema que ali está ocorrendo a criação de uma variável
+// Nesse exemplo, estamos criando um novo objeto e atribuindo seu valor e estado na variável objetoGerente
+// É possível também criar a variável com o tipo dela implicitamente ao invés de usar a palavra var
+// As variáveis têm uma importância enorme no mundo de desenvolvimento de software
+// Pois são nelas que guardamos, manipulamos, alteramos e exibimos valores para os usuários finais
+// Ou simplesmente fazemos cálculos e manipulações e enviamos talvez para outros sistemas que queiram se comunicar
+// De forma objetiva, a variável é meramente um nome dado para um armazenamento de dados que nosso programa vai manipular
+// Cada variável no C# possui um tipo específico que determina seu tamanho e o quanto de informação ela pode salvar na memória
