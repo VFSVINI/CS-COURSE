1d6f1d84890c6e2a76286644f3e3c812eef336d9
VFSVINI <vfs2@cin.ufpe.br>
2026-09-04 17:14:07 -0300

    Trabalhando Com Classes

---
+++ GerenteEX2.cs
@@ -0,0 +1,28 @@
+// Os métodos representam computacionalmente uma ação feita pelo ator na vida real
+// Dessa forma, vamos alterar um pouco nosso código
+// ncluir um método que vai possibilitar o gerente aprovar as horas de um funcionário informando o cpf do funcionário
+
+
+namespace SistemaBancario {
+
+    public class Gerente {
+
+        public string Nome {get; set;}
+        public int Idade {get; set;}
+        public int Cpf {get; set;}
+
+       public void AprovarHorasDeFuncionarios(int cpf){
+
+        // Lógica Para Aprovar Horas 
+
+       } 
+
+      
+    }
+}
+
+// A estrutura do código anterior representa uma classe no C#
+// uma representação do mundo real descrita através de um código fonte
+// onde o computador possa entender e executar as tarefas de forma automatizada
+// Esse processo feito até agora representa a modelagem de domínio do negócio
+// Normalmente em um projeto real é feito junto com especialistas que entendem do negócio em questão
